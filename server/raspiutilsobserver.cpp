0e157123fbabf40d103e823b836d85459cf7d4ed
WT <wojctrz@gmail.com>
2019-06-03 18:26:19 +0100

    Adding server support for 7seg display

--- server/raspiutilsobserver.cpp
+++ server/raspiutilsobserver.cpp
@@ -23,6 +23,8 @@ void RaspiUtilsObserver::update(uint8_t buff[], int size)
     std::cout << std::endl;
 
     led_flash(recv_buff[LED_BYTE]);
+
+    display(recv_buff[DISP_BYTE]);
 }
 
 void RaspiUtilsObserver::led_flash(uint8_t mode)
@@ -46,4 +48,113 @@ void RaspiUtilsObserver::led_flash(uint8_t mode)
             break;
 
     }
+}
+
+void RaspiUtilsObserver::display(uint8_t number)
+{
+	switch(number)
+	{
+		case 0:
+			digitalWrite(DISP_A, LOW);
+			digitalWrite(DISP_B, LOW);
+			digitalWrite(DISP_C, LOW);
+			digitalWrite(DISP_D, LOW);
+			digitalWrite(DISP_E, LOW);
+			digitalWrite(DISP_F, LOW);
+			digitalWrite(DISP_G, HIGH);
+			break;
+		case 1:
+			digitalWrite(DISP_A, HIGH);
+			digitalWrite(DISP_B, LOW);
+			digitalWrite(DISP_C, LOW);
+			digitalWrite(DISP_D, HIGH);
+			digitalWrite(DISP_E, HIGH);
+			digitalWrite(DISP_F, HIGH);
+			digitalWrite(DISP_G, HIGH);
+			break;
+		case 2:
+			digitalWrite(DISP_A, LOW);
+			digitalWrite(DISP_B, LOW);
+			digitalWrite(DISP_C, HIGH);
+			digitalWrite(DISP_D, LOW);
+			digitalWrite(DISP_E, LOW);
+			digitalWrite(DISP_F, HIGH);
+			digitalWrite(DISP_G, LOW);
+			break;
+		case 3:
+			digitalWrite(DISP_A, LOW);
+			digitalWrite(DISP_B, LOW);
+			digitalWrite(DISP_C, LOW);
+			digitalWrite(DISP_D, LOW);
+			digitalWrite(DISP_E, HIGH);
+			digitalWrite(DISP_F, HIGH);
+			digitalWrite(DISP_G, LOW);
+			break;
+		case 4:
+			digitalWrite(DISP_A, HIGH);
+			digitalWrite(DISP_B, LOW);
+			digitalWrite(DISP_C, LOW);
+			digitalWrite(DISP_D, HIGH);
+			digitalWrite(DISP_E, HIGH);
+			digitalWrite(DISP_F, LOW);
+			digitalWrite(DISP_G, LOW);
+			break;
+		case 5:
+			digitalWrite(DISP_A, LOW);
+			digitalWrite(DISP_B, HIGH);
+			digitalWrite(DISP_C, LOW);
+			digitalWrite(DISP_D, LOW);
+			digitalWrite(DISP_E, HIGH);
+			digitalWrite(DISP_F, LOW);
+			digitalWrite(DISP_G, LOW);
+			break;
+		case 6:
+			digitalWrite(DISP_A, LOW);
+			digitalWrite(DISP_B, HIGH);
+			digitalWrite(DISP_C, LOW);
+			digitalWrite(DISP_D, LOW);
+			digitalWrite(DISP_E, LOW);
+			digitalWrite(DISP_F, LOW);
+			digitalWrite(DISP_G, LOW);
+			break;
+		case 7:
+			digitalWrite(DISP_A, LOW);
+			digitalWrite(DISP_B, LOW);
+			digitalWrite(DISP_C, LOW);
+			digitalWrite(DISP_D, HIGH);
+			digitalWrite(DISP_E, HIGH);
+			digitalWrite(DISP_F, HIGH);
+			digitalWrite(DISP_G, HIGH);
+			break;
+		case 8:
+			digitalWrite(DISP_A, LOW);
+			digitalWrite(DISP_B, LOW);
+			digitalWrite(DISP_C, LOW);
+			digitalWrite(DISP_D, LOW);
+			digitalWrite(DISP_E, LOW);
+			digitalWrite(DISP_F, LOW);
+			digitalWrite(DISP_G, LOW);
+			break;
+		case 9:
+			digitalWrite(DISP_A, LOW);
+			digitalWrite(DISP_B, LOW);
+			digitalWrite(DISP_C, LOW);
+			digitalWrite(DISP_D, LOW);
+			digitalWrite(DISP_E, HIGH);
+			digitalWrite(DISP_F, LOW);
+			digitalWrite(DISP_G, LOW);
+			break;
+		default:
+			digitalWrite(DISP_A, HIGH);
+			digitalWrite(DISP_B, HIGH);
+			digitalWrite(DISP_C, HIGH);
+			digitalWrite(DISP_D, HIGH);
+			digitalWrite(DISP_E, HIGH);
+			digitalWrite(DISP_F, HIGH);
+			digitalWrite(DISP_G, HIGH);
+			break;
+	
+
+	}
+			
 }
--- server/raspiutilsobserver.hpp
+++ server/raspiutilsobserver.hpp
@@ -2,11 +2,25 @@
 #define RASPIUTILSOBSERVER_HPP
 
 #define LED_BYTE 1
+#define DISP_BYTE 2
 
 #define LED_PIN 0
 
+// define digital display pins
+#define DISP_A 1
+#define DISP_B 2
+#define DISP_C 3
+#define DISP_D 4
+#define DISP_E 5
+#define DISP_F 6
+#define DISP_G 7
+
 #define LED_OFF 0
 #define LED_ON 1
+
+// digital display us a common anode one, so it requires negative logic
+#define DISP_OFF 1
+#define DISP_ON 0
 
 #include "observer.hpp"
 #include <iostream>
@@ -28,6 +42,8 @@ private:
     void process_data();
 
     void led_flash(uint8_t mode);
+
+    void display(uint8_t number);
 
 };
 
